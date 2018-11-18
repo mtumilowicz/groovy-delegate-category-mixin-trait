7d991cc9e85098cf3d50626fcbbc1ef77910ab74
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-18 20:46:26 +0100

    mixins + tests

---
+++ src/main/groovy/mixins/StringUtils.groovy
@@ -0,0 +1,11 @@
+package mixins
+
+/**
+ * Created by mtumilowicz on 2018-11-18.
+ */
+ class StringUtils {
+     
+    static String concatWithComma(String self, String other) {
+        self + "," + other
+    }
+}
---
+++ src/test/groovy/mixins/StringUtilsTest.groovy
@@ -0,0 +1,18 @@
+package mixins
+
+import spock.lang.Specification
+
+/**
+ * Created by mtumilowicz on 2018-11-18.
+ */
+class StringUtilsTest extends Specification {
+    
+    def setupSpec() {
+        String.mixin(StringUtils)
+    }
+
+    def "string concat with comma"() {
+        expect:
+        "a".concatWithComma("b") == "a,b"
+    }
+}
